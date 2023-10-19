061dc22b1aa1cc8a9449abce1f178b925e8fbba7
Remzi Cavdar <ict@remzi.info>
2023-10-19 15:34:44 +0200

    Update

--- public/main.js
+++ public/main.js
@@ -1,20 +1,47 @@
 class DirectoryRenderer {
   #FILE_ICON_MAP = {
-    pdf: 'fa-regular fa-file-pdf',
-    image: 'fa-regular fa-file-image',
-    word: 'fa-regular fa-file-word',
-    excel: 'fa-regular fa-file-excel',
-    csv: 'fa-regular fa-file-csv',
-    js: 'bi bi-filetype-js',
-    json: 'bi bi-filetype-json',
-    html: 'bi bi-filetype-html',
-    css: 'bi bi-filetype-css',
-    sass: 'bi bi-filetype-sass',
-    scss: 'bi bi-filetype-scss',
-    xml: 'bi bi-filetype-xml',
-    ppt: 'bi bi-filetype-ppt',
-    pptx: 'bi bi-filetype-pptx',
-    default: 'fa-regular fa-file'
+    'folder': 'fa-regular fa-folder',
+    'pdf': 'fa-regular fa-file-pdf',
+    'image': 'fa-regular fa-file-image',
+    'word': 'fa-regular fa-file-word',
+    'excel': 'fa-regular fa-file-excel',
+    'csv': 'fa-regular fa-file-csv',
+    'js': 'bi bi-filetype-js',
+    'json': 'bi bi-filetype-json',
+    'html': 'bi bi-filetype-html',
+    'css': 'bi bi-filetype-css',
+    'sass': 'bi bi-filetype-sass',
+    'scss': 'bi bi-filetype-scss',
+    'xml': 'bi bi-filetype-xml',
+    'ppt': 'bi bi-filetype-ppt',
+    'pptx': 'bi bi-filetype-pptx',
+    'archive': 'bi bi-file-zip',
+    'default': 'fa-regular fa-file'
+  };
+
+  #EXTENSION_TO_TYPE_MAP = {
+    'zip': 'archive',
+    'rar': 'archive',
+    'tar': 'archive',
+    '7z': 'archive',
+    'pdf': 'pdf',
+    'png': 'image',
+    'jpg': 'image',
+    'jpeg': 'image',
+    'doc': 'word',
+    'docx': 'word',
+    'xls': 'excel',
+    'xlsx': 'excel',
+    'csv': 'csv',
+    'js': 'js',
+    'json': 'json',
+    'html': 'html',
+    'css': 'css',
+    'sass': 'sass',
+    'scss': 'scss',
+    'xml': 'xml',
+    'ppt': 'ppt',
+    'pptx': 'pptx'
   };
 
   constructor(containerSelector, JSON_URL) {
@@ -32,11 +59,23 @@ class DirectoryRenderer {
     return await response.json();
   }
 
-  #getFileIconClass(type) {
+  #getFileExtension(filename) {
+    const match = filename.match(/\.\w*$/);
+    return match ? match[0].slice(1).toLowerCase() : null;
+  }
+
+  #getFileType(filename) {
+    const extension = this.#getFileExtension(filename);
+    return this.#EXTENSION_TO_TYPE_MAP[extension] || 'default';
+  }
+
+  #getFileIconClass(filename) {
+    const type = this.#getFileType(filename);
     return this.#FILE_ICON_MAP[type] || this.#FILE_ICON_MAP.default;
   }
 
   #generateFolderHTML({name, children}) {
+    const cssClass = this.#FILE_ICON_MAP['folder'];
     const childrenHTML = children && children.length ? `
       <li class="list-group-item">
         <ul class="list-group">
@@ -45,13 +84,13 @@ class DirectoryRenderer {
       </li>` : '';
 
     return `
-      <li class="list-group-item"><i class="fa-regular fa-folder"></i> ${name}</li>
+      <li class="list-group-item"><i class="${cssClass}"></i> ${name}</li>
       ${childrenHTML}
     `;
   }
 
-  #generateFileHTML({type, path, name}) {
-    const cssClass = this.#getFileIconClass(type);
+  #generateFileHTML({path, name}) {
+    const cssClass = this.#getFileIconClass(name);  // Determine icon class based on the file's name
 
     return `
       <li class="list-group-item">
@@ -60,8 +99,8 @@ class DirectoryRenderer {
       </li>`;
   }
 
-  #generateHTML(directory) {
-    return directory.type === "folder" ? this.#generateFolderHTML(directory) : this.#generateFileHTML(directory);
+  #generateHTML(item) {
+    return item.path ? this.#generateFileHTML(item) : this.#generateFolderHTML(item);
   }
 
   #renderDirectories(directories) {
