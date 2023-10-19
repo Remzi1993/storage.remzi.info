3b80abda740b908c8d27f080db0230ea6d37758d
Remzi Cavdar <ict@remzi.info>
2023-10-19 12:14:58 +0200

    Update

--- public/main.js
+++ public/main.js
@@ -2,6 +2,18 @@ class DirectoryRenderer {
   #FILE_ICON_MAP = {
     pdf: 'fa-regular fa-file-pdf',
     image: 'fa-regular fa-file-image',
+    word: 'fa-regular fa-file-word',
+    excel: 'fa-regular fa-file-excel',
+    csv: 'fa-regular fa-file-csv',
+    js: 'bi bi-filetype-js',
+    json: 'bi bi-filetype-json',
+    html: 'bi bi-filetype-html',
+    css: 'bi bi-filetype-css',
+    sass: 'bi bi-filetype-sass',
+    scss: 'bi bi-filetype-scss',
+    xml: 'bi bi-filetype-xml',
+    ppt: 'bi bi-filetype-ppt',
+    pptx: 'bi bi-filetype-pptx',
     default: 'fa-regular fa-file'
   };
 
@@ -27,7 +39,7 @@ class DirectoryRenderer {
   #generateFolderHTML({name, children}) {
     const childrenHTML = children && children.length ? `
       <li class="list-group-item">
-        <ul class="list-group mb-2">
+        <ul class="list-group">
           ${children.map(child => this.#generateHTML(child)).join('')}
         </ul>
       </li>` : '';
